d92560498f602e314545d560254c81c91c3c12c6
Subham <subhamkumarshee@gmail.com>
2025-06-20 19:31:12 +0530

    added tmdb API key in .evng

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,51 +1,57 @@
-import React, { useEffect, useRef, useState } from 'react'
-import './TitleCards.css'
-import cards_data from '../../assets/cards/Cards_data'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react';
+import './TitleCards.css';
+import { Link } from 'react-router-dom';
 
-const TitleCards = ({title, category}) => {
-
+const TitleCards = ({ title, category }) => {
   const [apiData, setApiData] = useState([]);
-  const cardsRef= useRef();
+  const cardsRef = useRef();
+
+  const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
   const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMThhZWVhZDk2ZGQ2ZmUxNjVlNzQ1YzNhYTRlZmRlNCIsIm5iZiI6MTc1MDQwNjgwMS40NzcsInN1YiI6IjY4NTUxNjkxMWY5OTI0NTk5ZDBjZjcwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yGqGSTnWcVulj-Bmim1qtwPBCxCdnf6EDJ7E4HXrq58'
-  }
-};
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`,
+    },
+  };
 
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  };
 
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${category ? category : 'now_playing'}?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
 
-    const handleWheel = (event)=>{
-      event.preventDefault();
-      cardsRef.current.scrollLeft += event.deltaY;
-    }
+    const currentRef = cardsRef.current;
+    currentRef?.addEventListener('wheel', handleWheel);
 
-    useEffect(()=>{
-
-      fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
-  .then(res => res.json())
-  .then(res => setApiData(res.results))
-  .catch(err => console.error(err));
-
-      cardsRef.current.addEventListener('wheel', handleWheel);
-    },[])
+    return () => currentRef?.removeEventListener('wheel', handleWheel);
+  }, []);
 
   return (
-    <div className='title-cards'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
+    <div className="title-cards">
+      <h2>{title || 'Popular on Netflix'}</h2>
       <div className="card-list" ref={cardsRef}>
-        {apiData.map((card, index)=>{
-          return <Link to={`/player/${card.id}`} className="card" key={index}>
-            <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt=''/>
+        {apiData.map((card, index) => (
+          <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`}
+              alt={card.original_title}
+            />
             <p>{card.original_title}</p>
           </Link>
-        })}
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
